5546c24b06b8ae15c3849f73698ca2e99dd2a89f
KondapalliPurnaSekhar <36615694+KondapalliPurnaSekhar@users.noreply.github.com>
2024-01-01 13:54:37 +0530

    main.tf

--- Azure_Example_Full/main.tf
+++ Azure_Example_Full/main.tf
@@ -1,4 +1,4 @@
-
+/*
 #create resource group
 resource "azurerm_resource_group" "rg" {
     name     = "${var.rgname}"
@@ -81,3 +81,4 @@ resource "azurerm_windows_virtual_machine" "main" {
     caching              = "ReadWrite"
   }
 }
+*/
